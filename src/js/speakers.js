d4291fafd2479c3a12f2f0f492febb308952fd57
Sheersh01 <saxenasheersh1@gmail.com>
2025-09-21 05:20:40 +0530

    deploy43

--- src/js/speakers.js
+++ src/js/speakers.js
@@ -60,7 +60,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // Create card element
     const card = document.createElement("div");
     card.className =
-      "relative w-80 h-[440px] bg-white rounded-2xl overflow-hidden shadow-xl cursor-pointer group transition-all duration-1000";
+      "relative w-80 h-[470px] bg-white rounded-2xl overflow-hidden shadow-xl cursor-pointer group transition-all duration-1000";
 
     card.innerHTML = `
       <div class="absolute top-0 left-0 right-0 h-24 bg-gradient-to-br from-[#c3073f] to-[#950740] rounded-bl-[50%] rounded-br-[50%] transform -translate-y-24 transition-transform duration-1000 group-hover:translate-y-0"></div>
