4a1e2b8df998fe63d03a6ae8f4b901368c2141cd
Sheersh01 <saxenasheersh1@gmail.com>
2025-09-21 15:25:09 +0530

    deploy55

--- src/js/words.js
+++ src/js/words.js
@@ -3,26 +3,30 @@ import gsap from "gsap";
 const tl = gsap.timeline();
 
 tl.to("#words", {
-  delay:1,
+  delay: 1,
   opacity: 1,
-  duration:1.5,
+  duration: 1.5,
   ease: "power2.out",
 })
   .to("#explore", {
     color: "#c3073f", // red
-    duration: 1.2,
+    duration: 1.5,
   })
   .to(
     ["#the", "#unexplored"],
     {
       color: "#000000", // black
-      duration: 1.2,
+      duration: 1.5,
     },
     "-=1.0"
   ) // overlap a little
+  .to("#explore", {
+    color: "#000", // red
+    duration: 1.5,
+  })
   .to("#words", {
     opacity: 0,
-    duration:1.5,
+    duration: 1.5,
     ease: "power2.inOut",
   })
   .set("#words", { display: "none" });
